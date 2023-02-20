eeb7b69f26caae0b27f7ac11f808bbd9ffe0722c
Alfie Collins <alfiecollins44@gmail.com>
2023-02-20 15:07:14 +0000

    signup change

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,34 +1,27 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "./../context/auth.context";
 import { Input } from 'antd';
 import ClipLoader from "react-spinners/ClipLoader";
-import { useContext } from 'react'; 
 import { ThemeContext } from './../context/theme.context';
-import axios from "axios";
-
-
 import lightLogo from '../images/lightLogo.png'
 import darkLogo from '../images/darkLogo.png'
 
-
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
-
-function SignupPage(props) {
+function SignupPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const [loading, setLoading] = useState(false);
 
   const { theme } = useContext(ThemeContext);
 
-  const navigate = useNavigate();
+  const { storeToken, authenticateUser } = useContext(AuthContext);
 
-  
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
-  const handleName = (e) => setName(e.target.value);
+  const navigate = useNavigate();
 
   function changeLogoSrc(theme){
     if (theme === 'dark'){
@@ -36,63 +29,69 @@ function SignupPage(props) {
     } else {
        return lightLogo
     }
-};
+  };
 
-  
-  const handleSignupSubmit = (e) => {
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleConfirmPassword = (e) => setConfirmPassword(e.target.value);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Create an object representing the request body
-    const requestBody = { email, password, name };
+    const requestBody = { email, password, confirmPassword };
 
     setLoading(true);
 
-    // Make an axios request to the API
-    // If POST request is successful redirect to login page
-    // If the request resolves with an error, set the error message in the state
     axios.post(`${API_URL}/auth/signup`, requestBody)
       .then((response) => {
-        navigate("/login");
+        axios.post(`${API_URL}/auth/login`, { email, password })
+          .then((response) => {
+            storeToken(response.data.authToken);
+            authenticateUser();
+            navigate("/");
+          })
+          .catch((error) => {
+            console.log("Failed to log in user after sign up: ", error);
+            setErrorMessage("Failed to log in user after sign up");
+          });
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
       })
       .finally(() => {
-        // Set loading state back to false
         setLoading(false);
       });
   };
 
-  
   return (
     <div className={"signupPage " + theme}>
+      {loading ? <ClipLoader color="#36d7b7" className="clipLoader"/> : null}
 
-{loading ? <ClipLoader color="#36d7b7" className="clipLoader"/> : null}
+      <div id="signupInput">
+        <h1>Sign Up</h1>
 
-<div id="signupInput">
-      <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit} className='loginSignupForm'>
+          <label>Email:</label>
+          <Input type="email" name="email" value={email} onChange={handleEmail} />
 
-      <form onSubmit={handleSignupSubmit} className='loginSignupForm'>
-        <label>Email:</label>
-        <Input type="email" name="email" value={email} onChange={handleEmail} />
+          <label>Password:</label>
+          <Input type="password" name="password" value={password} onChange={handlePassword} />
 
-        <label>Password:</label>
-        <Input type="password" name="password" value={password} onChange={handlePassword} />
+          <label>Confirm Password:</label>
+          <Input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} />
 
-        <label>Name:</label>
-        <Input type="text" name="name" value={name} onChange={handleName} />
+          <button type="submit" className="loginSignupButton" >Sign Up</button>
+        </form>
 
-        <button type="submit" className="loginSignupButton">Sign Up</button>
-      </form>
+        { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
+        <p className="redirectMessages">Already have an account?</p>
+        <Link to={"/login"}> Login</Link>
+      </div>
 
-      <p className="redirectMessages">Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+      <img src={changeLogoSrc(theme)} alt="recipeLogo" className="logo"/>
     </div>
-    <img src={changeLogoSrc(theme)} alt="recipeLogo" className="logo"/>
-    </div>
-  )
+  );
 }
 
 export default SignupPage;
