0927f3c825b2617b556b3aa223d551ef11649fea
Alfie Collins <alfiecollins44@gmail.com>
2023-02-20 15:15:50 +0000

    the user now logs in automatically after signing up

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,19 +1,24 @@
-import { useState, useContext } from "react";
-import axios from "axios";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "./../context/auth.context";
 import { Input } from 'antd';
 import ClipLoader from "react-spinners/ClipLoader";
+import { useContext } from 'react'; 
 import { ThemeContext } from './../context/theme.context';
+import { AuthContext } from "./../context/auth.context";
+import axios from "axios";
+
+
 import lightLogo from '../images/lightLogo.png'
 import darkLogo from '../images/darkLogo.png'
 
+
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
-function SignupPage() {
+
+function SignupPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const [loading, setLoading] = useState(false);
 
@@ -23,75 +28,78 @@ function SignupPage() {
 
   const navigate = useNavigate();
 
+  
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleName = (e) => setName(e.target.value);
+
   function changeLogoSrc(theme){
     if (theme === 'dark'){
       return darkLogo
     } else {
        return lightLogo
     }
-  };
+};
 
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
-  const handleConfirmPassword = (e) => setConfirmPassword(e.target.value);
+  
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const requestBody = { email, password, name };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const requestBody = { email, password, confirmPassword };
+  setLoading(true);
 
-    setLoading(true);
+  axios.post(`${API_URL}/auth/signup`, requestBody)
+    .then((response) => {
+      axios.post(`${API_URL}/auth/login`, { email, password, name })
+        .then((response) => {
+          storeToken(response.data.authToken);
+          authenticateUser();
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log("Failed to log in user after sign up: ", error);
+          setErrorMessage("Failed to log in user after sign up");
+        });
+    })
+    .catch((error) => {
+      const errorDescription = error.response.data.message;
+      setErrorMessage(errorDescription);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+};
 
-    axios.post(`${API_URL}/auth/signup`, requestBody)
-      .then((response) => {
-        axios.post(`${API_URL}/auth/login`, { email, password })
-          .then((response) => {
-            storeToken(response.data.authToken);
-            authenticateUser();
-            navigate("/");
-          })
-          .catch((error) => {
-            console.log("Failed to log in user after sign up: ", error);
-            setErrorMessage("Failed to log in user after sign up");
-          });
-      })
-      .catch((error) => {
-        const errorDescription = error.response.data.message;
-        setErrorMessage(errorDescription);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
+  
   return (
     <div className={"signupPage " + theme}>
-      {loading ? <ClipLoader color="#36d7b7" className="clipLoader"/> : null}
 
-      <div id="signupInput">
-        <h1>Sign Up</h1>
+{loading ? <ClipLoader color="#36d7b7" className="clipLoader"/> : null}
 
-        <form onSubmit={handleSubmit} className='loginSignupForm'>
-          <label>Email:</label>
-          <Input type="email" name="email" value={email} onChange={handleEmail} />
+<div id="signupInput">
+      <h1>Sign Up</h1>
 
-          <label>Password:</label>
-          <Input type="password" name="password" value={password} onChange={handlePassword} />
+      <form onSubmit={handleSubmit} className='loginSignupForm'>
+        <label>Email:</label>
+        <Input type="email" name="email" value={email} onChange={handleEmail} />
 
-          <label>Confirm Password:</label>
-          <Input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} />
+        <label>Password:</label>
+        <Input type="password" name="password" value={password} onChange={handlePassword} />
 
-          <button type="submit" className="loginSignupButton" >Sign Up</button>
-        </form>
+        <label>Name:</label>
+        <Input type="text" name="name" value={name} onChange={handleName} />
 
-        { errorMessage && <p className="error-message">{errorMessage}</p> }
+        <button type="submit" className="loginSignupButton">Sign Up</button>
+      </form>
 
-        <p className="redirectMessages">Already have an account?</p>
-        <Link to={"/login"}> Login</Link>
-      </div>
+      { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-      <img src={changeLogoSrc(theme)} alt="recipeLogo" className="logo"/>
+      <p className="redirectMessages">Already have account?</p>
+      <Link to={"/login"}> Login</Link>
     </div>
-  );
+    <img src={changeLogoSrc(theme)} alt="recipeLogo" className="logo"/>
+    </div>
+  )
 }
 
 export default SignupPage;
