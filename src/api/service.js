1ddccaf67ce349db20f3e819824e46af8e8317aa
Alfie Collins <alfiecollins44@gmail.com>
2023-02-23 18:51:22 +0000

    change for cloudinary in deployed version

--- src/api/service.js
+++ src/api/service.js
@@ -4,7 +4,7 @@ const storedToken = localStorage.getItem('authToken');
 
 const api = axios.create({
   
-  baseURL: "http://localhost:5005/api" || process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/api` || "http://localhost:5005/api" ,
   headers:  { Authorization: `Bearer ${storedToken}` } 
   
 });
